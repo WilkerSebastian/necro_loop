fb65949621f463ab54298e68a2969d0bc30a43a0
WilkerSebastian <wilkerafonso2005@gmail.com>
2025-11-14 19:26:53 -0400

    feat: animação do rodolfo feita

--- Actors/Player/player.gd
+++ Actors/Player/player.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 
 @export var speed = 300.0
 
+@onready var anim_player = $AnimatedSprite2D
+
+const Combatant = preload("res://Actors/Units/Combatant.gd")
+
+var team = Combatant.Team.PLAYER
+
 func _process(delta: float):
 
 	var direction = Vector2.ZERO
@@ -22,8 +28,31 @@ func _process(delta: float):
 	
 	move_and_slide()
 	
+	update_animation()
+	
+	update_flip()
+	
 func take_damage(_damage_amount):
 	
 	print("Rodolfo foi atingido! Fim de Jogo.")
 	
 	get_tree().reload_current_scene()
+	
+func update_animation():
+	if velocity.length() > 0 and anim_player.animation != "run":
+		anim_player.play("run")
+			
+	else: 
+		if anim_player.animation != "idle":
+			anim_player.play("idle")
+
+func update_flip():
+	
+	if velocity.x == 0:
+		return
+		
+	if velocity.x > 0:
+		anim_player.flip_h = false
+		
+	elif velocity.x < 0:
+		anim_player.flip_h = true
--- Actors/Units/Warrior/Warrior.gd
+++ Actors/Units/Warrior/Warrior.gd
@@ -4,6 +4,8 @@ class_name Warrior
 @export var max_follow_distance: float = 150.0 
 @export var min_follow_distance: float = 75.0  
 
+var follow_target: CharacterBody2D
+
 var detection_area: Area2D
 
 func _ready():
@@ -23,38 +25,40 @@ func _ready():
 func _physics_process(_delta):
 	velocity = Vector2.ZERO
 	
-	if not is_instance_valid(target):
-		if team == Team.PLAYER:
-			_follow_player()
-		return
-		
+	if team == Team.PLAYER:
+		if is_instance_valid(target):
+			_run_combat_logic()
+		elif is_instance_valid(follow_target):
+			_run_follow_logic()
+	
+	elif team == Team.ENEMY:
+		if is_instance_valid(target):
+			_run_combat_logic()
+		else:
+			pass
+
+	move_and_slide()
+	
+func _run_combat_logic():
 	var distance_to_target = global_position.distance_to(target.global_position)
-		
+	
 	if distance_to_target > attack_range:
 		var direction = global_position.direction_to(target.global_position)
 		velocity = direction * speed
 	else:
 		velocity = Vector2.ZERO
 		if attack_timer.is_stopped():
-			_perform_attack() 
-			
-	move_and_slide()
-	
-func _follow_player():
-	
-	var player = get_tree().get_first_node_in_group("player")
-	
-	if not player: 
-		return
-	
-	var dir = global_position.direction_to(player.global_position)
-	var dist = global_position.distance_to(player.global_position)
+			_perform_attack()
+
+func _run_follow_logic():
+	var dir = global_position.direction_to(follow_target.global_position)
+	var dist = global_position.distance_to(follow_target.global_position)
 	
 	if dist > max_follow_distance:
 		velocity = dir * speed
-		
 	elif dist < min_follow_distance:
 		velocity = dir * -1 * speed
+	
 	
 func _perform_attack():
 	
@@ -62,13 +66,11 @@ func _perform_attack():
 		return
 
 	attack_timer.start()
-
 	var bodies = $MeleeHitbox.get_overlapping_bodies()
 	
 	for body in bodies:
 		if body == target:
 			if body.has_method("take_damage"):
-				
 				var is_same_team = false
 				
 				if "team" in body:
@@ -76,13 +78,14 @@ func _perform_attack():
 
 				if not is_same_team:
 					body.take_damage(attack_damage)
-					break 
-				
+					break
+		return
+	
 func _on_enemy_entered(body):
 	if team == Team.PLAYER and body.is_in_group("enemies"):
-		if target == null or target.is_in_group("player"):
+		if not is_instance_valid(target):
 			target = body
 
 func _on_enemy_exited(body):
 	if body == target:
-		target = get_tree().get_first_node_in_group("player")
+		target = null
