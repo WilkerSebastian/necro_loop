9221252387ce5b270f2b4c4c2ee6fec8f2b1900d
WilkerSebastian <wilkerafonso2005@gmail.com>
2025-11-14 05:52:33 -0400

    feat: sistema de horda basico feito

--- Actors/Units/Combatant.gd
+++ Actors/Units/Combatant.gd
@@ -3,10 +3,17 @@ extends CharacterBody2D
 
 enum Team { PLAYER, ENEMY }
 
-@export var ally_color: Color = Color.BLUE
-@export var enemy_color: Color = Color.RED
+enum EnemyState { IDLE, ATTACKING }
+
+var enemy_state = EnemyState.IDLE
+var horde_spawn_position = Vector2.ZERO 
+
+var target = null
 
 var visual_node: Node
+
+@export var ally_color: Color = Color.BLUE
+@export var enemy_color: Color = Color.RED
 
 @export var team: Team = Team.ENEMY:
 	set(value):
@@ -19,8 +26,6 @@ var visual_node: Node
 @export var health: float = 100.0
 @export var speed: float = 150.0
 @export var attack_damage: float = 10.0
-
-var target = null
 
 func _ready():
 	visual_node = $Visual
@@ -49,8 +54,35 @@ func _update_visuals():
 	#        visual_node.texture = load("res://path/to/enemy_sprite.png")
 
 
-func set_team(new_team: Team):
-	self.team = new_team
+func command_attack(new_target):
+	target = new_target
+	if target:
+		enemy_state = EnemyState.ATTACKING
+	else:
+		command_idle()
+
+func command_idle(idle_position = horde_spawn_position):
+	target = null
+	enemy_state = EnemyState.IDLE
+	horde_spawn_position = idle_position 
+
+func _set_team_value(new_team):
+	team = new_team
+	
+	if team == Team.PLAYER:
+		add_to_group("allies")
+		remove_from_group("enemies")
+  
+		collision_layer = 1 << 2 # Camada 3
+		collision_mask = 1 | (1 << 3) # Camadas 1 e 4
+	else:
+		add_to_group("enemies")
+		remove_from_group("allies")
+		collision_layer = 1 << 3 # camada 4
+		collision_mask = 1 | (1 << 2) # camadas 1 e 3
+
+	if is_inside_tree():
+		_update_visuals()
 
 func take_damage(damage_amount):
 	health -= damage_amount
--- Actors/Units/Warrior/Warrior.gd
+++ Actors/Units/Warrior/Warrior.gd
@@ -5,27 +5,35 @@ class_name Warrior
 @export var min_follow_distance: float = 75.0  
 
 func _ready():
-	super._ready()
+	super._ready() 
 	if team == Team.PLAYER:
 		target = get_tree().get_first_node_in_group("player")
+	else:
+		command_idle(global_position)
 
 func _physics_process(_delta):
-	
 	velocity = Vector2.ZERO
 
-	if team == Team.PLAYER and target:
+	if team == Team.PLAYER:
+		if target:
+			var dir = global_position.direction_to(target.global_position)
+			var dist = global_position.distance_to(target.global_position)
+			if dist > max_follow_distance:
+				velocity = dir * speed
+			elif dist < min_follow_distance:
+				velocity = dir * -1 * speed
 		
-		var direction_to_target = global_position.direction_to(target.global_position)
-		var distance_to_target = global_position.distance_to(target.global_position)
-
-		if distance_to_target > max_follow_distance:
-			velocity = direction_to_target * speed
-		
-		elif distance_to_target < min_follow_distance:
-			velocity = direction_to_target * -1 * speed
-
 	elif team == Team.ENEMY:
-		# TODO: Logica de IA do inimigo
-		pass 
-	
+		match enemy_state:
+			EnemyState.IDLE:
+				# TODO: Poderia andar aleatoriamente perto da 'horde_spawn_position'
+				pass 
+			
+			EnemyState.ATTACKING:
+				if is_instance_valid(target): 
+					var direction = global_position.direction_to(target.global_position)
+					velocity = direction * speed
+				else:
+					command_idle()
+
 	move_and_slide()
